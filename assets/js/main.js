61914fb91cbf3250eff71acbc5604a57a7ccd0aa
Rasel Rana Earshad <charry.br10@gmail.com>
2024-08-09 03:37:30 +0600

    Added mixitUp Plugin

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// MixitUp Plugin Settings
+var mixer = mixitup('.mixcontainer', {
+  animation: {
+    enable: true,
+    effects: 'fade translateY(100%)',
+    duration: 800,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    perspectiveDistance: '2000px',
+  },
+});
